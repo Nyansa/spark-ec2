e7d697c8e6f66b002c36f7942753d5ddf742a428
Jason Cheok Wan <jason@nyansa.com>
2018-07-18 15:59:28 -0700

    Adding scala 2.11 support to spark 1.6.3 ec2 deployment

--- deploy_templates.py
+++ deploy_templates.py
@@ -71,6 +71,7 @@ template_vars = {
   "spark_worker_cores": "%d" %  worker_cores,
   "spark_master_opts": os.getenv("SPARK_MASTER_OPTS", ""),
   "spark_version": os.getenv("SPARK_VERSION"),
+  "scala_version": os.getenv("SCALA_VERSION"),
   "tachyon_version": os.getenv("TACHYON_VERSION"),
   "hadoop_major_version": os.getenv("HADOOP_MAJOR_VERSION"),
   "hadoop_tmp_dir": os.getenv("HADOOP_TMP_DIR"),
