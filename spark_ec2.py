0dd6addbb5b9ca1379f60f4c0f8dbd12a046658b
Daniel Kan <dan@nyansa.com>
2018-10-02 19:27:28 -0700

    Continue to use nyansa-2.0 branch

--- spark_ec2.py
+++ spark_ec2.py
@@ -111,7 +111,7 @@ DEFAULT_SPARK_GITHUB_REPO = "https://github.com/apache/spark"
 
 # Default location to get the spark-ec2 scripts (and ami-list) from
 DEFAULT_SPARK_EC2_GITHUB_REPO = "https://github.com/Nyansa/spark-ec2"
-DEFAULT_SPARK_EC2_BRANCH = "nyansa-2.0_scala_211"
+DEFAULT_SPARK_EC2_BRANCH = "nyansa-2.0"
 
 
 def setup_external_libs(libs):
